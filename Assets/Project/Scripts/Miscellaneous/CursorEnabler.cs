67e570f37864d4d42915752b6b170ac04b09ffa6
sabari <developer@Devdensolutions.com>
2022-12-16 12:45:32 +0530

    first commit for PC

---
+++ Assets/Project/Scripts/Miscellaneous/CursorEnabler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorEnabler : MonoBehaviour
+{
+    public void EnableCursor(bool val)
+    {
+        //Cursor.visible = val;
+    }
+}
